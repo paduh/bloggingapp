63a2fb74792f6a33e97c386dfab605fc229713ec
Perfect Aduh <paduh@getcarcon.co>
2021-07-19 06:24:59 +0100

    Added post model

---
+++ bloggingapp/Model/Post.swift
@@ -0,0 +1,30 @@
+//
+//  Post.swift
+//  bloggingapp
+//
+//  Created by Aduh Perfect on 19/07/2021.
+//
+
+import Foundation
+
+// MARK:- Post
+
+struct Post: Codable {
+
+    let userId:Int
+    let id: Int
+    let title: String
+    let body: String
+    
+    init(
+        userId: Int,
+        id: Int,
+        title: String,
+        body: String
+    ) {
+        self.userId = userId
+        self.id = id
+        self.title = title
+        self.body = body
+    }
+}
