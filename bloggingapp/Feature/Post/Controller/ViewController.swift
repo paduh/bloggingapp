eea1625fba579fc332256ebc0e4548e2994ec1ba
Perfect Aduh <paduh@getcarcon.co>
2021-07-19 06:04:50 +0100

    Folder structure organization

---
+++ bloggingapp/Feature/Post/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bloggingapp
+//
+//  Created by Aduh Perfect on 19/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
