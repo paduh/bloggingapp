fb8d20ebbec2b8b174025c6f392882eec3ec778a
Perfect Aduh <paduh@getcarcon.co>
2021-07-19 07:07:53 +0100

    Added app string for all strings

---
+++ bloggingapp/AppString/Text.swift
@@ -0,0 +1,28 @@
+//
+//  Text.swift
+//  bloggingapp
+//
+//  Created by Aduh Perfect on 19/07/2021.
+//
+
+import Foundation
+
+// MARK: - Text
+
+struct Text: RawRepresentable {
+    
+    var rawValue: String
+    
+    init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+extension Text {
+    static let ok = Text(rawValue: "Ok")
+    static let league = Text(rawValue: "Blogging App")
+    static let cancel = Text(rawValue: "Cancel")
+    static let unknownError = Text(rawValue: "Unknown Error")
+    static let cannotDequeueheader = Text(rawValue: "Could not dequeue header cell with type")
+    static let cannotDequeueCell = Text(rawValue: "Could not dequeue cell with type")
+}
