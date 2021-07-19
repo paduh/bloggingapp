8fa5094243850ac4a13c756a65888aa463ccd02d
Perfect Aduh <paduh@getcarcon.co>
2021-07-19 07:02:53 +0100

    added post item

---
+++ bloggingapp/Feature/Post/Model/PostItem.swift
@@ -0,0 +1,27 @@
+//
+//  PostItem.swift
+//  bloggingapp
+//
+//  Created by Aduh Perfect on 19/07/2021.
+//
+
+import Foundation
+
+// MARK:- PostItem
+
+final class PostItem {
+    
+    // MARK: Properties
+    
+    let title: String
+    let body: String
+    let model: Post
+    
+    // MARK:- Initialiser
+    
+    init(_ model: Post) {
+        self.model = model
+        self.title = model.title
+        self.body = model.body
+    }
+}
