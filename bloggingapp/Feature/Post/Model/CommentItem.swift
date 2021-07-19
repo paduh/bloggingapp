0d366f1305553dbe935ee5ccd679b2168e9fa321
Perfect Aduh <paduh@getcarcon.co>
2021-07-19 17:14:05 +0100

    implement comment  item

---
+++ bloggingapp/Feature/Post/Model/CommentItem.swift
@@ -0,0 +1,29 @@
+//
+//  CommentItem.swift
+//  bloggingapp
+//
+//  Created by Aduh Perfect on 19/07/2021.
+//
+
+import Foundation
+
+// MARK: - CommentItem
+
+final class CommentItem {
+    
+    // MARK: Properties
+
+    let email: String
+    let title: String
+    let body: String
+    let model: Comment
+    
+    // MARK:- Initialiser
+    
+    init(_ model: Comment) {
+        self.model = model
+        email = model.email
+        title = model.name
+        body = model.body
+    }
+}
